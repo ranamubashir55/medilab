af09dd29c510e57f489c8b6cd29fbbf28267ed20
unknown <mubashir.rana@ne.com.pk>
2021-06-09 01:27:39 +0500

    admin updated

--- app.py
+++ app.py
@@ -46,11 +46,83 @@ def add_patient():
             path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             f.save(path)
         conn = sqlite3.connect('database.db')
-        query = "INSERT into patients(firstname , lastname , address , contact ,age ,  gender , status ,patient_category , stool , sensitivity , taste , thirst , hands_feet_burn , hunger , sleep ,constitution, attachment)"
-        query =query+f"VALUES ('{request.form['inputFirstName']}', '{request.form['inputLastName']}', '{request.form['address']}', '{request.form['contact']}', '{request.form['age']}', '{request.form['gender']}', '{request.form['status']}', '{request.form['p_category']}', '{request.form['stool']}', '{request.form['sensitivity']}', '{request.form['taste']}', '{request.form['thirst']}', '{request.form['burn_feel']}', '{request.form['hunger']}', '{request.form['sleep']}', '{request.form['Constitution']}', '{filename}')"
+        query = "INSERT into patients(firstname , lastname , address , contact ,age ,  gender , status ,patient_category , stool , sensitivity , taste , thirst , hands_feet_burn , hunger , sleep ,constitution, attachment, symptoms)"
+        query =query+f"VALUES ('{request.form['inputFirstName']}', '{request.form['inputLastName']}', '{request.form['address']}', '{request.form['contact']}', '{request.form['age']}', '{request.form['gender']}', '{request.form['status']}', '{request.form['p_category']}', '{request.form['stool']}', '{request.form['sensitivity']}', '{request.form['taste']}', '{request.form['thirst']}', '{request.form['burn_feel']}', '{request.form['hunger']}', '{request.form['sleep']}', '{request.form['Constitution']}', '{filename}', '{request.form['symptoms']}')"
         conn.execute(query)
         conn.commit()
         return render_template('add_patient.html', data={"msg":"Patient data added successfully.."})
+
+@app.route("/add_visit", methods=['GET','POST','PUT'])
+def add_visit():
+    if 'id' not in session:
+        return render_template("login.html")
+    if request.method== 'GET':
+        v_id = request.args.get("v_id")
+        p_id = request.args.get("id")
+        if v_id:
+            conn = sqlite3.connect('database.db')
+            record = conn.execute(f"select * from patient_visit where id= {str(v_id)}")
+            data = record.fetchone()
+            return render_template("add_visit.html", v_data = data, vid= v_id)
+        if p_id:
+            return render_template("add_visit.html", p_id = p_id)
+
+    if request.method=="POST":
+        now = d.now()
+        date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+        p_id = request.args.get("id")
+        v_id = request.args.get("v_id")
+        symp = request.form['symptoms']
+        diagnosis = request.form['diagnosis']
+        med = request.form['medicine']
+        feedback = request.form['feedback']
+        fee = request.form['fee']
+        is_repeat = request.form['repeat']
+        p_id = is_repeat if is_repeat else p_id
+        conn = sqlite3.connect('database.db')
+        if p_id or is_repeat:
+            query = f"Insert into patient_visit(patient_id, symptoms, diagnosis, medicine, fee, date, feedback) Values ({p_id}, '{symp}','{diagnosis}', '{med}', '{fee}','{date_time}', '{feedback}')"
+            message = "Succss! Patient visit added successfully.. "
+            conn.execute(query)
+            conn.commit()
+            return redirect(url_for('patient_detail', id=int(p_id)))
+        if v_id and not is_repeat:
+            query = f"Update patient_visit set symptoms='{symp}',diagnosis='{diagnosis}', medicine='{med}', fee='{fee}', date='{date_time}', feedback='{feedback}'  where id = {int(v_id)}"
+            message = "Patient visit info has been updated successfully.. "
+            conn.execute(query)
+            conn.commit()
+            return render_template("add_visit.html", msg={"msg":message,"class":"alert-success"})
+    
+        
+        
+
+@app.route("/patient_detail", methods= ['GET','POST'])
+def patient_detail():
+    if 'id' not in session:
+        return render_template("login.html")
+    if request.method=="GET":
+        conn = sqlite3.connect('database.db')
+        patient_id = request.args.get('id')
+        p_records = conn.execute(f"Select * from patients where id = {patient_id}")
+        p_details = p_records.fetchone()
+        records = conn.execute(f"Select * from patient_visit where patient_id = {patient_id}")
+        data = records.fetchall()
+        print(data)
+        return render_template("view_patient_detail.html", data=data, p_id = patient_id, patient_detail = p_details)
+    if request.method=="POST":
+        conn = sqlite3.connect('database.db')
+        patient_id = int(request.args.get('id'))
+        f = request.files['docs']
+        filename=''
+        if f:
+            filename = secure_filename(f.filename)
+            path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            f.save(path)
+        attach = f",attachment='{filename}'" if filename else ''
+        query = f"Update patients set age ='{request.form['age']}', contact='{request.form['contact']}', status='{request.form['marritial']}', sensitivity='{request.form['sensitive']}', taste='{request.form['taste']}', thirst='{request.form['thirst']}', hands_feet_burn='{request.form['burning']}',hunger='{request.form['hunnger']}', sleep='{request.form['sleep']}',patient_category='{request.form['p_category']}',stool='{request.form['stool']}',constitution='{request.form['constitution']}',address='{request.form['address']}'"+attach+f" where id={patient_id}"
+        conn.execute(query)
+        conn.commit()
+        return redirect( url_for('view_patient'))
 
 @app.route("/send_file", methods=['POST'])
 def get_file():
@@ -83,6 +155,49 @@ def settings():
         else:
             return render_template("change_pass.html", data={"msg":"Error! Check your old password.. ", "class":"alert-danger"})
 
+@app.route('/add_combination', methods=['POST', 'GET'])
+def add_combination():
+    if 'id' not in session:
+        return render_template("login.html")
+    id = session['id']
+    if request.method=='GET':
+        if request.args.get('id'):
+            conn = sqlite3.connect('database.db')
+            query=f"Select * from combination where id= {request.args['id']}"
+            records = conn.execute(query)
+            data = records.fetchone()
+            print(data)
+            return render_template('add_combination.html', data = data )
+        else:
+            return render_template("add_combination.html")
+    if request.method == 'POST':
+        disease = request.form['disease']
+        medicine = request.form['medicine']
+        record_id = request.args.get('id')
+        conn = sqlite3.connect('database.db')
+        if record_id:
+            query = f"Update combination set disease='{disease}', medicine='{medicine}' where id= {record_id} "
+            message = "Combination updated successfully.."
+        if not record_id:
+            query = f"Insert into combination (disease, medicine) Values ('{disease}', '{medicine}')"
+            message = "New combination added successfully.."
+        try:
+            conn.execute(query)
+            conn.commit()
+            return render_template('add_combination.html', msg={"msg":message, "class":"alert-success"})
+        except Exception:
+            return render_template('add_combination.html', msg={"msg":"Warning! Combiation already exists..", "class":"alert-danger"})
+
+@app.route('/del_combination', methods=['GET'])
+def del_combination():
+    if 'id' not in session:
+        return render_template("login.html")
+    if request.method=="GET":
+        conn = sqlite3.connect('database.db')
+        conn.execute(f"delete from combination where id= {request.args.get('id')}")
+        conn.commit()
+        return redirect(url_for('view_combination'))
+
 @app.route('/view_patient' , methods=['GET'])
 def view_patient():
     
@@ -95,6 +210,19 @@ def view_patient():
         data = records.fetchall()
         print(data)
         return render_template("view_patient.html", data=data)
+
+@app.route('/view_combination' , methods=['GET'])
+def view_combination():
+    if 'id' not in session:
+        return render_template("login.html")
+    id = session['id']
+    if request.method=="GET":
+        conn = sqlite3.connect('database.db')
+        records = conn.execute("Select * from combination")
+        data = records.fetchall()
+        print(data)
+        return render_template("view_combination.html", data=data)
+
 
 @app.route('/logout')
 def logout():
--- db.py
+++ db.py
@@ -1,11 +1,13 @@
 a = [
 "CREATE TABLE if not exists users (ID INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT UNIQUE, password TEXT)",
 # "INSERT INTO users (username, password) VALUES ('admin', 'admin')",
-"CREATE TABLE if not exists patients (ID INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT, lastname TEXT, address TEXT, contact number,age TEXT, patient_category TEXT, gender TEXT, status Text, stool TEXT, sensitivity TEXT, taste TEXT, thirst TEXT, hands_feet_burn TEXT, hunger TEXT, sleep TEXT,constitution TEXT, attachment TEXT )"
+"CREATE TABLE if not exists patients (ID INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT, lastname TEXT, address TEXT, contact number,age TEXT, patient_category TEXT, gender TEXT, status Text, stool TEXT, sensitivity TEXT, taste TEXT, thirst TEXT, hands_feet_burn TEXT, hunger TEXT, sleep TEXT,constitution TEXT, attachment TEXT,symptoms TEXT )",
+"Create table if not exists combination (ID INTEGER PRIMARY KEY AUTOINCREMENT, disease TEXT UNIQUE, medicine TEXT )",
+"Create Table if not exists patient_visit (ID INTEGER PRIMARY KEY AUTOINCREMENT, patient_id INTEGER, symptoms text, diagnosis text, medicine text, fee text, date text, feedback text)"
 ]
 import sqlite3
 conn = sqlite3.connect('database.db')
-# import pdb;pdb.set_trace()
+import pdb;pdb.set_trace()
 for each in a:
     records = conn.execute(each)
 conn.commit()
