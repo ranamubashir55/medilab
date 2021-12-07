185a41e930c9a3d108a44813468b42c14ae064da
unknown <mubashir.rana@ne.com.pk>
2021-12-08 01:48:09 +0500

    latest

--- app.py
+++ app.py
@@ -269,4 +269,4 @@ def login():
 
 if __name__ =="__main__":
     # app.run(host= "0.0.0.0", debug=True ,port=80, threaded=True)
-    app.run(host= "0.0.0.0",port=5000,debug=True, threaded=True)
+    app.run(host= "0.0.0.0",port=8080,debug=True, threaded=True)
